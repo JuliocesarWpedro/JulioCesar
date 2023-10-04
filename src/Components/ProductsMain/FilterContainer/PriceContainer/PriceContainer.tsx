899402269d139bb14c960711ee6481d57640bcbb
Júlio César <juliowanke@gmail.com>
2023-10-04 14:25:25 -0300

    refactor: PriceContainer input

--- src/Components/ProductsMain/FilterContainer/PriceContainer/PriceContainer.tsx
+++ src/Components/ProductsMain/FilterContainer/PriceContainer/PriceContainer.tsx
@@ -3,20 +3,33 @@ import { prices } from '../../../../ts/Products';
 import { useFilter } from '../../../../Contexts/FilterContext';
 
 const PriceContainer = () => {
-  const { setSelectedPrice } = useFilter();
+  const { selectedPrice, setSelectedPrice } = useFilter();
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedValue = JSON.parse(event.target.value);
+
+    if (
+      selectedPrice &&
+      selectedValue.minPrice === selectedPrice.minPrice &&
+      selectedValue.maxPrice === selectedPrice.maxPrice
+    ) {
+      setSelectedPrice(null); // Desmarca o checkbox se ele já estiver selecionado
+    } else {
+      setSelectedPrice(selectedValue); // Marca o checkbox selecionado
+    }
+  };
 
   return (
     <>
       {prices.map((price) => (
         <div className={styles.priceContainer} key={price.value}>
           <input
-            type="radio"
+            type="checkbox"
             id={String(price.minPrice)}
             name="price"
+            checked={selectedPrice?.value === price.value}
             value={JSON.stringify(price)}
-            onChange={(event) =>
-              setSelectedPrice(JSON.parse(event.target.value))
-            }
+            onChange={handleCheckboxChange}
           />
           <label htmlFor={String(price.minPrice)}>{price.value}</label>
         </div>
