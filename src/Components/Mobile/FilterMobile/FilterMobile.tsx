750b0d21218262c1addd0e15489416d49e1e6489
Júlio César <juliowanke@gmail.com>
2023-10-05 14:16:35 -0300

    feat: FilterMobile

--- src/Components/Mobile/FilterMobile/FilterMobile.tsx
+++ src/Components/Mobile/FilterMobile/FilterMobile.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './FilterMobile.module.scss';
+import { useFilter } from '../../../Contexts/FilterContext';
 
 const FilterMobile = ({
   setOpenModalFilter,
@@ -8,9 +9,20 @@ const FilterMobile = ({
   setOpenModalFilter: React.Dispatch<React.SetStateAction<boolean>>;
   setOpenModalOrder: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const { setSelectedPrice, setSelectedColors, setSelectedSizes } = useFilter();
+
   return (
     <div className={styles.filterMobile}>
-      <button onClick={() => setOpenModalFilter(true)}>Filtrar</button>
+      <button
+        onClick={() => {
+          setOpenModalFilter(true),
+            setSelectedPrice(null),
+            setSelectedColors([]),
+            setSelectedSizes([]);
+        }}
+      >
+        Filtrar
+      </button>
       <button onClick={() => setOpenModalOrder(true)}>Ordenar</button>
     </div>
   );
