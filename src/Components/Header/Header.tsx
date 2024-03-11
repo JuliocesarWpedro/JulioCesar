a9dd32228e310a42f6282ec3abe064b02360245e
Júlio César Wanderosfky Pedro <juliowanke@gmail.com>
2024-03-11 19:26:14 -0300

    change logo title

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,4 +1,3 @@
-import imageLogo from '../../img/logo-m3.png';
 import CartIcon from '../../img/Icons/CartIcon.svg';
 import styles from './Header.module.scss';
 import { useCart } from '../../Contexts/CartContext';
@@ -8,18 +7,13 @@ const Header = () => {
 
   return (
     <header className={styles.header}>
-      <a href="https://m3ecommerce.com/">
-        <img src={imageLogo} alt="Imagem Logo" />
+      <a href="/">
+        <h2>Clothing Store</h2>
       </a>
       <div onClick={() => toggleCart()} className={styles.cart}>
         <img src={CartIcon} alt="Cart Icon" />
         <div className={styles.cartCount}>
-          <span>
-            {cartItems.reduce(
-              (accumulator, current) => (accumulator += current.quantity),
-              0,
-            )}
-          </span>
+          <span>{cartItems.length}</span>
         </div>
       </div>
     </header>
