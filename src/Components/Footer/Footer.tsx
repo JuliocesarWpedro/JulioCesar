096ee6f2700f7991b27a7bcd45d5b1e49e951b89
Júlio César Wanderosfky Pedro <juliowanke@gmail.com>
2024-03-11 19:25:50 -0300

    change: title and footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.scss';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>M3: Implantação de E-commerce VTEX</p>
+      <p>Clothing Store</p>
     </footer>
   );
 };
